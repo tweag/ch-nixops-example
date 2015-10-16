b82f0cdfeb921a6289cc367106b67d4170938e84
YPares <yves.pares@gmail.com>
2015-10-16 18:49:17 +0200

    Added distributed-process as a dep

--- default.nix
+++ default.nix
@@ -1,10 +1,14 @@
-{ mkDerivation, base, directory, filepath, stdenv }:
+{ mkDerivation, base, directory, distributed-process, filepath
+, stdenv
+}:
 mkDerivation {
   pname = "dp-nixops-example";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base directory filepath ];
+  executableHaskellDepends = [
+    base directory distributed-process filepath
+  ];
   license = stdenv.lib.licenses.bsd3;
 }
