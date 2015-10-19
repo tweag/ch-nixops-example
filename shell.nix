eb6e1428ae4e3e0a1619862abdbfeb2e65b588fc
Yves Parès (Ywen) <yves.pares@gmail.com>
2015-10-19 17:47:09 +0200

    Update shell.nix
    Added a link to useful tips about configuring the shell.nix

--- shell.nix
+++ shell.nix
@@ -1,3 +1,4 @@
 { haskellPackages ? (import <nixpkgs> {}).haskellPackages }:
 (haskellPackages.callPackage (import ./.) {}).env
 
+# See http://nixos.org/nixpkgs/manual/#how-to-create-nix-builds-for-your-own-private-haskell-packages
