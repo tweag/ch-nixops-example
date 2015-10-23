6e0e53a4099111f95048a2ef9cfc87803ad2a383
Arnaud Spiwack <arnaud@spiwack.net>
2015-10-24 01:12:32 +0200

    NixOps example: processes as toplevel functions.
    Prepares eventual dispatch between machines.

--- Main.hs
+++ Main.hs
@@ -8,22 +8,32 @@ import Network.Transport.TCP (createTransport, defaultTCPParameters)
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
 
+-- | @ping d pid1 pid@ sends a message to @pid@ and waits for a
+-- reply. @d@ and @pid1@ are temporary logging arguments.
+ping :: FilePath -> ProcessId -> ProcessId -> Process ()
+ping d pid1 pid2 = do
+  say $ "Dir: " ++ d
+  say $ show pid1
+  send pid2 "hello"
+  hello <- expect :: Process String
+  say $ "Received: " ++ hello
+  liftIO $ appendFile (d </> "plop.txt" ) (hello++"\n")
+
+-- | @pong pid1 pid2$ waits for a message and replies to
+-- @pid1@. @pid2@ is a temporary logging argument.
+pong :: ProcessId -> ProcessId -> Process ()
+pong pid1 pid2 = do
+  say $ show pid2
+  msg <- expect :: Process String
+  send pid1 $ "You said: " ++ msg
+
 main :: IO ()
 main = do
   d <- getHomeDirectory
   Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
   node <- newLocalNode t initRemoteTable
-  (mdo pid1 <- forkProcess node $ do
-         say $ "Dir: " ++ d
-         say $ show pid1
-         send pid2 "hello"
-         hello <- expect :: Process String
-         say $ "Received: " ++ hello
-         liftIO $ appendFile (d </> "plop.txt" ) (hello++"\n")
-       pid2 <- forkProcess node $ do
-         say $ show pid2
-         msg <- expect :: Process String
-         send pid1 $ "You said: " ++ msg
+  (mdo pid1 <- forkProcess node $ ping d pid1 pid2
+       pid2 <- forkProcess node $ pong pid1 pid2
        return ())
   runProcess node $ do _ <- expect :: Process ()
                        return ()
