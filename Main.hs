ab5ef5c218761cd8cf1faac994b8b32090bfd908
Arnaud Spiwack <arnaud@spiwack.net>
2015-10-28 16:03:23 +0100

    NixOps example: add a second machine

--- Main.hs
+++ Main.hs
@@ -3,14 +3,25 @@
 module Main where
 
 import Control.Monad
+import System.Environment (getArgs)
 import System.Directory (getHomeDirectory)
 import System.FilePath
 import Data.Time.Clock (getCurrentTime)
 import Data.Time.Format (formatTime, defaultTimeLocale)
+import qualified Data.ByteString.Char8 as ByteStringChar
+import qualified Network.Transport as NT
 import Network.Transport.TCP (createTransport, defaultTCPParameters)
 import Control.Distributed.Process
 import Control.Distributed.Process.Closure
 import Control.Distributed.Process.Node
+
+
+-- | Names of the machines on the network
+helloworlder :: String
+helloworlder = "helloworlder"
+
+ponger :: String
+ponger = "ponger"
 
 -- | @ping d pid1 pid@ sends a message to @pid@ and waits for a
 -- reply. @d@ and @pid1@ are temporary logging arguments.
@@ -54,19 +65,52 @@ remotable ['ping, 'pong, 'logger]
 -- /boilerplate
 --
 
-main :: IO ()
-main = do
+-- | Port on which the nodes of the cluster will listen
+port :: String
+port = "10501"
+
+-- | @makeNodeId addr@ creates a 'NodeId' to represent the process
+-- which runs on the machine at address @addr@.
+makeNodeId :: String -> NodeId
+makeNodeId addr =
+  NodeId . NT.EndPointAddress . ByteStringChar.pack $ addr ++ ":" ++ port ++ ":0"
+
+-- | 'NodeId' of the nodes on the cluster
+helloworlderid :: NodeId
+helloworlderid = makeNodeId helloworlder
+
+pongerid :: NodeId
+pongerid = makeNodeId ponger
+
+
+
+dispatch :: [String] -> RemoteTable -> IO ()
+dispatch [] rtable = do
   d <- getHomeDirectory
-  Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
+  Right t <- createTransport helloworlder port defaultTCPParameters
   node <- newLocalNode t rtable
-  let nodeid = localNodeId node
   runProcess node $ do
-    logpid <- spawn nodeid $ $(mkClosure 'logger) (d </> "hwer.log")
+    logpid <- spawn helloworlderid $ $(mkClosure 'logger) (d </> "hwer.log")
     reregister "logger" logpid
     say $ "Dir: " ++ d
-    pongpid <- spawn nodeid $ $(mkStaticClosure 'pong)
-    _ <- spawn nodeid $ $(mkClosure 'ping) pongpid
+    say $ show helloworlderid
+    say $ show pongerid
+    pongpid <- spawn pongerid $ $(mkStaticClosure 'pong)
+    say "pong spawned"
+    _ <- spawn helloworlderid $ $(mkClosure 'ping) pongpid
+    say "hwer spawned"
     expect
+dispatch ["--node"] rtable = do
+  Right t <- createTransport ponger port defaultTCPParameters
+  node <- newLocalNode t rtable
+  runProcess node expect
+dispatch _ _ = do
+  ioError $ userError "Invalid arguments"
+
+main :: IO ()
+main = do
+  args <- getArgs
+  dispatch args rtable
   where
     rtable :: RemoteTable
     rtable = __remoteTable initRemoteTable
--- config-nixops.nix
+++ config-nixops.nix
@@ -1,5 +1,6 @@
 {
   network.description = "Haskell + NixOps FTW!";
+
   helloworlder = { config, pkgs, ... }:
     let hwer = pkgs.haskellPackages.callPackage (import ./.) {};
     in
@@ -18,5 +19,24 @@
           ExecStart = ''${hwer}/bin/${hwer.pname}'';
         };
       };
+      networking.firewall.enable = false;
+    };
+
+  ponger = { config, pkgs, ... }:
+    let hwer = pkgs.haskellPackages.callPackage (import ./.) {};
+    in
+    {
+      deployment.targetEnv = "virtualbox";
+      deployment.virtualbox.headless = true;
+
+      systemd.services.hwernode = {
+        wantedBy = ["multi-user.target"];
+        serviceConfig = {
+          Type = "simple";
+          User = "root";
+          ExecStart = ''${hwer}/bin/${hwer.pname} --node'';
+        };
+      };
+      networking.firewall.enable = false;
     };
 }
